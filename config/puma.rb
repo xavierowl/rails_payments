9a40a49e864ceabae1d8c168f01724bede40a4fe
bcalva@territoriosintelitenges.net <bcalva@territoriosinteligentes.net>
2025-08-20 22:42:56 -0500

    Heroku: Procfile + puma tuned

--- config/puma.rb
+++ config/puma.rb
@@ -1,34 +1,44 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
+# This configuration file will be evaluated by Puma. The top-level methods that are invoked here are part of Puma's 
+# configuration DSL. For more information about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
+# Puma starts a configurable number of processes (workers) and each process serves each request in a thread from an 
+# internal thread pool.
 #
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
+# The ideal number of threads per worker depends both on how much time the application spends waiting for IO operations 
+# and on how much you wish to to prioritize throughput over latency.
 #
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
+# As a rule of thumb, increasing the number of threads will increase how much traffic a given process can handle 
+# (throughput), but due to CRuby's Global VM Lock (GVL) it has diminishing returns and will degrade the response time 
+# (latency) of the application.
 #
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
+# The default is set to 3 threads as it's deemed a decent compromise between throughput and latency for the average Rails 
+# application.
 #
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-threads threads_count, threads_count
+# Any libraries that use a connection pool or another resource pool should be configured to provide at least as many 
+# connections as the number of threads. This includes Active Record's `pool` parameter in `database.yml`.
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Threads
+max_threads = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+min_threads = Integer(ENV.fetch("RAILS_MIN_THREADS", max_threads))
+threads min_threads, max_threads
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# Workers (processes)
+workers Integer(ENV.fetch("WEB_CONCURRENCY", 2))
+
+# Puerto y entorno (Heroku setea PORT y RAILS_ENV=production)
+port        Integer(ENV.fetch("PORT", 3000))
+environment ENV.fetch("RAILS_ENV", "production")
+
+# Arranque más rápido y menor RAM por worker
+preload_app!
+
+# Reconectar DB por worker (muy importante en Heroku)
+on_worker_boot do
+  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+end
+
+# Permitir restart con bin/rails restart (opcional)
 plugin :tmp_restart
 
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
+# PID opcional si lo usas
 pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
